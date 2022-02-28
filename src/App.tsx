4ff7a42718e564d3531c94866a22b32c2514026b
Amir Titelman <amirti7@gmail.com>
2022-02-28 23:19:04 +0200

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,150 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
-import styled from "styled-components"
+import styled from "styled-components";
 import Rows from "./components/Rows";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { ImLoop } from "react-icons/im";
+import { FaRegStopCircle } from "react-icons/fa";
 
 const Container = styled.div`
-height:60vh;
-width:60vw;
-background-color:lightgrey;
-margin: auto;
-
-`;
-const Title = styled.h1`
+  height: 60vh;
+  width: 40vw;
+  background-color: lightgrey;
+  margin: auto;
 `;
 
+const Title = styled.h1``;
+
+const LoopingStatement = styled.p`
+  color: red;
+`;
+export interface RowAudio {
+  src: string;
+  on: boolean;
+}
+
+const initState: RowAudio[] = [
+  {
+    src: "DRUMS",
+    on: true,
+  },
+  {
+    src: "B VOC",
+    on: true,
+  },
+  {
+    src: "ALL TRACK",
+    on: true,
+  },
+  {
+    src: "_tambourine_shake_higher",
+    on: true,
+  },
+  {
+    src: "JIBRISH",
+    on: true,
+  },
+  {
+    src: "LEAD 1",
+    on: true,
+  },
+  {
+    src: "UUHO VOC",
+    on: true,
+  },
+  {
+    src: "HE HE VOC",
+    on: true,
+  },
+];
 export default function App() {
+  const [rowAudios, setRowAudios] = useState<RowAudio[]>(initState);
+  const [value, setValue] = useState<number>(0);
+  const [isLooping, setIsLooping] = useState<boolean>(false);
+
+  function playSongs() {
+    let audios = document.getElementsByTagName("audio");
+    var audiosArr = Array.prototype.slice.call(audios, 0);
+    console.log("audiosArr", audiosArr);
+    audiosArr.forEach((audio) => {
+      audio.play();
+    });
+    progressBarUpdate();
+  }
+
+  function stopMusic() {
+    let audios = document.getElementsByTagName("audio");
+    var audiosArr = Array.prototype.slice.call(audios, 0);
+    audiosArr.forEach((audio) => {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.loop = false;
+    });
+    stopProgressBarUpdate();
+  }
+
+  function stopProgressBarUpdate() {
+    setValue(18);
+  }
+
+  function loopMusic() {
+    setIsLooping(!isLooping);
+    let audios = document.getElementsByTagName("audio");
+    var audiosArr = Array.prototype.slice.call(audios, 0);
+    audiosArr.forEach((audio) => {
+      if (!audio.muted) {
+        if (!audio.loop) audio.loop = true;
+        else audio.loop = false;
+      }
+    });
+  }
+
+  function progressBarUpdate() {
+    const interval = setInterval(() => {
+      setValue((oldValue) => {
+        const newValue = oldValue + 1;
+        if (newValue >= 18) {
+          clearInterval(interval);
+          setValue(0);
+          console.log("hey1");
+        }
+        return newValue;
+      });
+    }, 1000);
+  }
+
   return (
     <Container className="App">
-     <Title>Media Player</Title>
-     <Rows/>
-     <button>play</button>
+      <Title>Looper</Title>
+      <Rows rows={rowAudios} />
+      <audio id="DRUMS" src="/audio/DRUMS.mp3"></audio>
+      <audio id="B VOC" src="/B VOC.mp3"></audio>
+      <audio id="ALL TRACK" src="/audio/ALL TRACK.mp3"></audio>
+      <audio
+        id="_tambourine_shake_higher"
+        src="/audio/_tambourine_shake_higher.mp3"
+      ></audio>
+      <audio id="JIBRISH" src="/audio/JIBRISH.mp3"></audio>
+      <audio id="LEAD 1" src="/audio/LEAD 1.mp3"></audio>
+      <audio id="UUHO VOC" src="/audio/UUHO VOC.mp3"></audio>
+      <audio id="HE HE VOC" src="/audio/HE HE VOC.mp3"></audio>
+      {"00:" + value}
+      <progress value={value} max={18}></progress>
+      {"0:18"}
+      <br></br>
+      <button onClick={playSongs}>
+        <AiFillPlayCircle />
+      </button>
+      <button onClick={stopMusic}>
+        <FaRegStopCircle />
+      </button>
+      <button onClick={loopMusic}>
+        <ImLoop />
+      </button>
+      <br></br>
+      <LoopingStatement>
+        {isLooping ? "Looping" : "Not Looping"}
+      </LoopingStatement>
     </Container>
   );
 }
--- src/components/Rows.tsx
+++ src/components/Rows.tsx
@@ -1,23 +1,76 @@
 import styled from "styled-components";
+import { RowAudio } from "../App";
 
+const colors = [
+  "maroon",
+  "purple",
+  "blue",
+  "darkgreen",
+  "aqua",
+  "blueviolet",
+  "chocolate",
+  "crimson",
+  "deeppink",
+];
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  text-align: left;
+`;
+const MenuMute = styled.span`
+  float: right;
+`;
+
+const MenuName = styled.span`
+  text-align: left;
 `;
 const Row = styled.div`
   width: 100%;
-  height: 20%;
+  height: 10%;
 `;
 
-export default function Rows() {
+interface props {
+  rows: RowAudio[];
+}
+
+const Rows: React.FC<props> = ({ rows }) => {
+  function changeIsMuted(rowName: string) {
+    let audios = document.getElementsByTagName("audio");
+    var audiosArr = Array.prototype.slice.call(audios, 0);
+    let selectedAudio = audiosArr.find((audio) => audio.id == rowName);
+    if (!selectedAudio.muted) selectedAudio.muted = true;
+    else selectedAudio.muted = false;
+  }
   return (
     <Container>
-      <audio id="myAudio">
-        <source src="horse.ogg" type="audio/ogg" />
-        <source src="horse.mp3" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-      <Row>Row1</Row>
+      <MenuName>
+        <b>
+          <u>Channel Name</u>
+        </b>
+      </MenuName>
+      <MenuMute>
+        <b>
+          <u>Mute Channel</u>
+        </b>
+      </MenuMute>
+      {rows.map((row) => (
+        <Row
+          style={{
+            backgroundColor: `${colors.at(rows.indexOf(row))}`,
+          }}
+        >
+          {row.src}&nbsp;&nbsp;
+          <input
+            style={{ float: "right" }}
+            type="checkbox"
+            onChange={() => changeIsMuted(row.src)}
+          />
+          {/* <label>Mute</label> */}
+          <br></br>
+        </Row>
+      ))}
     </Container>
   );
-}
+};
+
+export default Rows;
